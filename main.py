e9536b9e8830229d99683ca27abfebd900ab6509
Din-Don-Dan <danielisjacopo@gmail.com>
2025-07-25 23:49:13 +0200

    basic test using example in Sirachi et al.

---
+++ main.py
@@ -0,0 +1,64 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import bispy as bp #https://github.com/fandreuz/BisPy
+
+coupling_edges = [[0,1],[0,2],[1,2],[3,2],[3,4],[4,2]]
+Coupling = nx.DiGraph()
+Coupling.add_edges_from(coupling_edges)
+
+#Coupling_scc = nx.condensation(Coupling)
+#print(Coupling_scc.number_of_nodes())
+
+circuit_edges = [[5,7],[5,8],[6,7],[7,8]]
+Circuit = nx.DiGraph()
+Circuit.add_edges_from(circuit_edges)
+
+#Circuitscc = nx.condensation(Circuit)
+#print(Circuit_scc.number_of_nodes())
+
+#subax1 = plt.subplot(121)
+#nx.draw(Coupling, with_labels=True, font_weight='bold')
+#plt.show()
+
+#subax2 = plt.subplot(121)
+#nx.draw(Circuit, with_labels=True, font_weight='bold')
+#plt.show()
+
+
+roots = []
+for x in Circuit.nodes:
+    if not Circuit.in_degree(x):
+        roots.append(x)
+
+for x in Coupling.nodes:
+    if not Coupling.in_degree(x):
+        roots.append(x)
+print(roots)
+
+BG = nx.union(Coupling, Circuit)
+rt = max(BG.nodes)+1
+for x in roots:
+    BG.add_edge(rt, x)
+
+subax3 = plt.subplot(111)
+nx.draw(BG, with_labels=True, font_weight='bold')
+
+
+print(bp.dovier_piazza_policriti(BG, is_integer_graph=True))
+
+plt.show()
+
+# 5 a0, 6 a1, 7 b0, 8 b1
+
+# mapping esatto da https://hal.science/hal-01655951/file/Siraichi_QubitAllocation_CGO18.pdf:
+# 0 -> 6, 1 -> x, 2 -> 7, 3 -> 5, 4 -> 8
+
+# coupling:
+# [0,1],[0,2],[1,2],[3,2],[3,4],[4,2]
+
+# mapped circuit interactions after Bis [(2, 8), (1, 4, 7), (6,), (0, 3, 5), (9,)]:
+# 5 -> 3, 6 -> 0, 7 -> 4, 8 -> 2. (9 artificiale rimosso) 
+# [5,7],[5,8],[6,7],[7,8] in
+# [3,2],[3,4],[0,2],[2,4]
+
+# effettivamente si introduce solo il reversal in 2,4
