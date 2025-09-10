4133a32061991637f978c49def0ead17d0c2ed84
Din-Don-Dan <danielisjacopo@gmail.com>
2025-09-10 22:22:56 +0200

    Refactored code structure, basic time test implemented

--- main.py
+++ main.py
@@ -1,82 +1,57 @@
+import random
+import a_utils
 import networkx as nx
-import bispy as bp #https://github.com/fandreuz/BisPy
-import queue
-#import matplotlib.pyplot as plt
+import time
+import matplotlib.pyplot as plt
 
-# Ipotesi che Coupling e Circuit siano aciclici a priori (nessun problema di dimensioni di scc)
-# nodi Coupling da 0 a n
-# nodi Circuit da n+1 a m
+#system clock estimate
+def resolution():
+    start = time.monotonic()
+    while time.monotonic() == start:
+        pass
+    stop = time.monotonic()
+    return stop - start
 
-coupling_edges = [[0,1],[0,2],[1,2],[3,2],[3,4],[4,2]]
-Coupling = nx.DiGraph()
-Coupling.add_edges_from(coupling_edges)
-n = max(Coupling.nodes)
-#Coupling_scc = nx.condensation(Coupling)
-#print(Coupling_scc.number_of_nodes())
+def benchmark(input1, input2, func, resolution, maxError = 0.001):
+    tmin = resolution * (1 + (1 / maxError))
+    count = 0
+    start = time.monotonic()
 
-circuit_edges = [[5,7],[5,8],[6,7],[7,8]]
-Circuit = nx.DiGraph()
-Circuit.add_edges_from(circuit_edges)
+    while time.monotonic() - start < tmin:
+        random.seed(2**count)
+        func(input1, input2)
+        count += 1
+    duration = time.monotonic() - start
+    return  duration / count
 
-#Circuitscc = nx.condensation(Circuit)
-#print(Circuit_scc.number_of_nodes())
+if __name__ == "__main__":
+    resolution = resolution()
 
-roots = []
-for x in Circuit.nodes:
-    if not Circuit.in_degree(x):
-        roots.append(x)
+    # add weights to all the edges of all Couplings 
+    a_utils.add_weights(a_utils.devices_coupling_maps_dict)
 
-for x in Coupling.nodes:
-    if not Coupling.in_degree(x):
-        roots.append(x)
-#print(roots)
+    i = 0
+    points = [(None,None)] * len(a_utils.devices_coupling_maps_dict.keys())
+    for x, y in a_utils.devices_coupling_maps_dict.items():
+        print(x)
+        Coupling = nx.DiGraph()
+        Coupling.add_weighted_edges_from(y)
+        Coupling.remove_edges_from(a_utils.filter(Coupling))
 
-BG = nx.union(Coupling, Circuit)
-rt = max(BG.nodes)+1
-for x in roots:
-    BG.add_edge(rt, x)
+        n = max(Coupling.nodes)
+        circuit_edges = [[n+1,n+3],[n+1,n+4],[n+2,n+3],[n+3,n+4]] #[[1,3],[1,4],[2,3],[3,4]]
+    
+        points[i] = (x,
+                     benchmark(Coupling, circuit_edges, a_utils.bismap, resolution))
+        i += 1
 
-bis = bp.dovier_piazza_policriti(BG, is_integer_graph=True)
+    xs, ys1= zip(*points)
+    plt.figure()
+    plt.scatter(xs, ys1)
 
-# mapping esatto da https://hal.science/hal-01655951/file/Siraichi_QubitAllocation_CGO18.pdf:
-# 0 -> 6, 1 -> x, 2 -> 7, 3 -> 5, 4 -> 8
-
-#come decidere il mapping:
-coupling_stack = queue.LifoQueue(maxsize=n+1)
-circuit_stack = queue.LifoQueue(maxsize=rt-n)
-mapping = []
-
-bis.reverse()
-
-for bisset in bis:
-    for node in bisset: #scorre m+1 nodi, lineare non quadratico
-        if node == rt:
-            continue
-        if node <= n:
-            if not circuit_stack.empty():
-                candidate = circuit_stack.get()
-                mapping.append([candidate, node])
-            else:
-                coupling_stack.put(node)
-        if node > n:
-            if not coupling_stack.empty():
-                candidate = coupling_stack.get()
-                mapping.append([node, candidate])
-            else:
-                circuit_stack.put(node)
-
-mdict = dict(mapping)
-print("Mapping qubit {logico : fisico} :", mdict)
-
-mapped = []
-for x in circuit_edges:
-    mapped.append(list(map(mdict.get, x)))
-print("Circuito mappato:", mapped)
-
-# debug:
-l = []
-while not coupling_stack.empty():
-    l.append(coupling_stack.get())
-    print("qubit fisici non utilizzati:" ,l)
-if not circuit_stack.empty():
-    print("Errore - Circuit stack non vuota")
+    plt.title('Average mapping time')
+    plt.xlabel('Coupling architecture')
+    plt.ylabel('Time (s)')
+    plt.legend()
+    plt.savefig('grafico.png')
+    plt.show()
